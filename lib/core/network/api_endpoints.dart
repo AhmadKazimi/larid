c4a039476bd1b8512636e05b94fc0352dc8d85cf
Kazimi <ahmad.kazmi@startappz.com>
2025-02-17 17:04:26 +0300

    add get endpoints

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,6 +1,14 @@
 class ApiEndpoints {
   // Auth endpoints
   static const String checkUser = '/CheckUser';
+  static const String getCustomers = '/GetCustomers';
+  static const String getSalesrepRouteCustomers = '/GetSalesrepRout';
+  static const String getCustomersPriceList = '/GetCustomersPriceList';
+  static const String getInventoryItems = '/GetIVItems';
+  static const String getInventoryUnits = '/GetIVUnits';
+  static const String getUserWearhouse = '/GetUserWH';
+  static const String getSalesTaxes = '/GetSalesTaxes';
+  static const String uploadPayment = '/UploadPayment';
 
   // Helper method to build full endpoint URL
   static String buildUrl(String endpoint) {
